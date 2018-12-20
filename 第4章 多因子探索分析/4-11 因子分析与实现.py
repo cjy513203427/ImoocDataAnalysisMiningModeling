e50d92876da8bf0d3dfeae028f845ff63d213aa3
cjy513203427 <634208959@qq.com>
2018-12-20 16:29:41 +0800

    深究Adaboost，因子分析美化输出

--- 第4章 多因子探索分析/4-11 因子分析与实现.py
+++ 第4章 多因子探索分析/4-11 因子分析与实现.py
@@ -1,15 +1,23 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+# 防止pandas输出结果省略
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_colwidth', 1000)
 
 df = pd.read_csv("./data/hr.csv")
 df = df.dropna(axis=0, how="any")
 df = df[df["last_evaluation"] <= 1][df["salary"] != "nme"][df["department"] != "sale"]
 
 from sklearn.decomposition import PCA
+from sklearn import preprocessing
 my_pca = PCA(n_components=7)
-lower_mat = my_pca.fit_transform(df.drop(labels=["salary","department","left"],axis=1))
+df = df.drop(labels=["salary","department","left"],axis=1)
+lower_mat = my_pca.fit_transform(df)
 
 print("Ratio:\n",my_pca.explained_variance_ratio_)
+data_scaled = pd.DataFrame(preprocessing.scale(df),columns = df.columns)
+print("Column\n",data_scaled)
 sns.heatmap(pd.DataFrame(lower_mat).corr(),vmin=-1,vmax=1,cmap=sns.color_palette("RdBu",n_colors=128))
 plt.show()
--- 第4章 多因子探索分析/4-7 编码实现.py
+++ 第4章 多因子探索分析/4-7 编码实现.py
@@ -18,37 +18,41 @@ print("t检验大样本\n", ss.ttest_ind(ss.norm.rvs(size=100), ss.norm.rvs(size
 # 方差检验
 print("方差检验\n", ss.f_oneway([49, 50, 39, 40, 43], [28, 32, 30, 26, 24], [38, 40, 45, 42, 48]))
 
-s1 = pd.Series([0.1,0.2,1.1,2.4,1.3,0.3,0.5])
-s2 = pd.Series([0.5,0.4,1.2,2.5,1.1,0.7,0.1])
-#相关系数
-print("相关系数\n",s1.corr(s2))
-print("spearman相关系数\n",s1.corr(s2,method="spearman"))
+s1 = pd.Series([0.1, 0.2, 1.1, 2.4, 1.3, 0.3, 0.5])
+s2 = pd.Series([0.5, 0.4, 1.2, 2.5, 1.1, 0.7, 0.1])
+# 相关系数
+print("相关系数\n", s1.corr(s2))
+print("spearman相关系数\n", s1.corr(s2, method="spearman"))
 
-df = pd.DataFrame([s1,s2])
+df = pd.DataFrame([s1, s2])
 print(df.corr())
-#reshape成m行n列
-x = np.arange(10).astype(np.float).reshape(10,1)
-print("x是\n",x)
-#自己构造一个函数，用来检测线性回归的结果
-y=x*3+4+np.random.random((10,1))
-print("y是\n",y)
+# reshape成m行n列
+x = np.arange(10).astype(np.float).reshape(10, 1)
+print("x是\n", x)
+# 自己构造一个函数，用来检测线性回归的结果
+y = x * 3 + 4 + np.random.random((10, 1))
+print("y是\n", y)
 
-#线性回归
+# 线性回归
 from sklearn.linear_model import LinearRegression
-reg = LinearRegression()
-reg = reg.fit(x,y)
-y_pred = reg.predict(x)
-print("参数\n",reg.coef_)
-print("截距\n",reg.intercept_)
 
-data = np.array([np.array([2.5,0.5,2.2,1.9,3.1,2.3,2,1,1.5,1.1]),
-            np.array([2.4,0.7,2.9,2.2,3,2.7,1.6,1.1,1.6,0.9])]).T
-print("data是\n",data)
+reg = LinearRegression()
+reg = reg.fit(x, y)
+y_pred = reg.predict(x)
+print("参数\n", reg.coef_)
+print("截距\n", reg.intercept_)
+
+data = np.array([np.array([2.5, 0.5, 2.2, 1.9, 3.1, 2.3, 2, 1, 1.5, 1.1]),
+                 np.array([2.4, 0.7, 2.9, 2.2, 3, 2.7, 1.6, 1.1, 1.6, 0.9])]).T
+print("data是\n", data)
 
 from sklearn.decomposition import PCA
+
+# n_components:Number of components to keep. if n_components is not set all components are kept:
 lower_dim = PCA(n_components=1)
 lower_dim.fit(data)
-#explained_variance_ratio_它代表降维后的各主成分的方差值占总方差值的比例，这个比例越大，则越是重要的主成分
-print("降维后结果\n",lower_dim.explained_variance_ratio_)
+# explained_variance_ratio_它代表降维后的各主成分的方差值占总方差值的比例，这个比例越大，则越是重要的主成分
+print("降维后结果\n", lower_dim.explained_variance_ratio_)
 
-print("计算Transformed Data\n",lower_dim.transform(data))
+print("主成分Transformed Data\n", lower_dim.transform(data))
+
